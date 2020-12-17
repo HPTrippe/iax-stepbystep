da2578e63be3ed1af654145a5c93d0f9fc69b840
HPTrippe <72393473+HPTrippe@users.noreply.github.com>
2020-12-17 10:57:11 +0000

    Form builder changes

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
+router.post('/eligibility-step-results', function (req, res) {
+ var answers = req.session.data;
+ for (const [question, answer] of Object.entries(answers)) {
+  if (answer == 'no') {
+   // Redirect if any answer is 'no'
+   res.redirect('/eligibility-step-results-no')
+  }
+ }
+ // If all answers are 'yes'
+ res.redirect('/eligibility-step-results-yes')
+})
+
 // Add your routes here - above the module.exports line
 
 module.exports = router
