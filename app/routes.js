55b6bd083347786325f17e436d6527174f758739
HPTrippe <72393473+HPTrippe@users.noreply.github.com>
2020-12-17 16:33:20 +0000

    rules error fix

--- app/routes.js
+++ app/routes.js
@@ -13,29 +13,5 @@ router.post('/eligibility-step-results', function (req, res) {
  res.redirect('/eligibility-step-results-yes')
 })
 
-//router.post('/YOUR-PAGE-PATH-HERE', function (req, res) {
-  let errorSummary = []
-  let errors = {}
-  const selectedOption = req.body['are-details']
-  if (!selectedOption) {
-    const message = '[Select either of these options to proceed]'
-    errorSummary = [
-      {
-        text: message,
-        href: '#are-details-conditional'
-      }
-    ]
-    errors['are-details'] = {
-      text: message
-    }
-  }
-  if (errorSummary.length > 0) {
-    return res.render('/start', { errorSummary, errors })
-  } else {
-    res.redirect('/[NEXT_QUESTION_PATH]')
-  }
-});
-
- Add your routes here - above the module.exports line
 
 module.exports = router
